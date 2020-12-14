c9cf013f8481ac7ca65bf53adae5d1b11fea92a4
opiumpopp <524185788@qq.com>
2020-12-14 20:34:59 +0800

    Update UUIDUtil.java

--- src/com/yougou/util/UUIDUtil.java
+++ src/com/yougou/util/UUIDUtil.java
@@ -1,17 +1,4 @@
-/*
- * 类名 UUIDUtils.java
- *
- * 版本信息 
- *
- * 日期 2014年3月21日
- *
- * 版权声明Copyright (C) 2014 YouGou Information Technology Co.,Ltd
- * All Rights Reserved.
- * 本软件为优购科技开发研制，未经本公司正式书面同意，其他任何个人、团体不得
- * 使用、复制、修改或发布本软件。
- */
 package com.yougou.util;
-
 
 import java.util.UUID;
 
